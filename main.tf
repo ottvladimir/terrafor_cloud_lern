31065180019be46642c1e97291f2d8342edef342
ottvladimir <77604827+ottvladimir@users.noreply.github.com>
2021-08-17 09:23:41 +1000

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,12 @@
+    provider "aws" {
+      region = "us-west-2"
+    }
+
+    data "aws_ami" "ubuntu" {
+      owners = ["amazon"]
+      most_recent = true
+      filter {
+            name   = "name"
+            values = ["*ubuntu*"]
+            }
+    }
