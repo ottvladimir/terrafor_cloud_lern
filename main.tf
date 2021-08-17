7e8721e7a26cb91fb6de27054c45cb5875f981d2
ottvladimir <ottvvl@gmail.com>
2021-08-17 00:06:25 +0000

    R  backend.tf -> backend.tf.mv M  main.tf

--- main.tf
+++ main.tf
@@ -21,14 +21,3 @@ resource "aws_instance" "web" {
     name = "simpleserver-${terraform.workspace}"
    }
 }
-resource "aws_instance" "test" {
-  ami = data.aws_ami.amazon_linux.id
-  instance_type = local.web_instance_type_map[terraform.workspace]
-  count = local.instances_count[terraform.workspace]
-  tags = {
-    name = "testforeachserver-${terraform.workspace}"
-   }
-}
-resource "aws_s3_bucket" "web_states" {
-  bucket = "ovv-terraform-states"
-}
